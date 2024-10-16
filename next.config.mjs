dff27b8ffcdc641f3bf3d16ef7bd7f7313558147
sacredstarfamily <seeloveinfinite@gmail.com>
2024-10-15 20:28:05 -0700

    add wallet

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // output: 'export',
+  webpack: config => {
+    config.externals.push('pino-pretty', 'lokijs', 'encoding')
+    return config
+  }
+};
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
+'use client';
 import Image from "next/image";
 import Navbar from './components/Navbar';
+import {useAccount} from 'wagmi';
+
+
 export default function Home() {
+ 
+const {isConnected} = useAccount();
   return (
     <>
     <Navbar/>
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-walto-graph)]">
      
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-       
+      <w3m-button />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
@@ -61,6 +67,9 @@ export default function Home() {
           />
           Learn
         </a>
+        {isConnected ? (
+         <p>Connected</p>
+        ) : null}
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
