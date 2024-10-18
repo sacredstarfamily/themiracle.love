c79b5db905fd7b54f0deff9f288aa28f91b0e390
sacredstarfamily <seeloveinfinite@gmail.com>
2024-10-18 13:42:41 -0700

    j

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,13 +1,10 @@
 import { NextResponse } from "next/server";
-import conn from "../lib/db";
+//import conn from "../lib/db";
 
 
 export async function GET() {
-   const client = await conn.connect();
-   const result = await client.query('CREATE TABLE products (product_id serial PRIMARY KEY, name VARCHAR(50), price DOUBLE PRECISION)');
-   const results = { 'results': (result) ? result.rows : null};
-    client.release();
-    return NextResponse.json({ message: results }, { status: 200 })
+  
+    return NextResponse.json({ message:"p" }, { status: 200 })
  }
 
 export async function POST() {
