39c0ce2b71d3c5447d2e10a63d21f5d3a4bed33a
sacredstarfamily <seeloveinfinite@gmail.com>
2024-10-18 13:29:52 -0700

    j

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -4,7 +4,7 @@ import conn from "../lib/db";
 
 export async function GET() {
    const client = await conn.connect();
-   const result = await client.query('CREATE TABLE users (id serial PRIMARY KEY, name VARCHAR(50), email VARCHAR(50))');
+   const result = await client.query('CREATE TABLE users (id serial PRIMARY KEY, firstname VARCHAR(50), email VARCHAR(50), walletAddress VARCHAR(100))');
    const results = { 'results': (result) ? result.rows : null};
 
     return NextResponse.json({ message: results }, { status: 200 })
