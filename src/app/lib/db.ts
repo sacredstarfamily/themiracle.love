094c1cf53b49b3f707aaef1d9c1c627ddbaee125
sacredstarfamily <seeloveinfinite@gmail.com>
2024-10-18 07:53:55 -0700

    pool

--- src/app/lib/db.ts
+++ src/app/lib/db.ts
@@ -1,12 +1,13 @@
 import {Pool} from 'pg';
 
-
-const pool = new Pool({
+let conn; 
+if(!conn){
+conn = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
-  password: process.env.DB_PASSORD,
+  password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
 });
-
-module.exports = pool;
+}
+export default conn;
