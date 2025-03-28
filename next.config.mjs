39cc7d3528325f7212a2e4c1b71a7443d36518d2
sacredstarfamily <seeloveinfinite@gmail.com>
2025-03-28 12:48:09 -0700

    yp

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,9 @@ const nextConfig = {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
   },
+  env: {
+    PAYPAL_CLIENT_SECRET: process.env.PAYPAL_CLIENT_SECRET
+  }
 };
 
 export default nextConfig;
